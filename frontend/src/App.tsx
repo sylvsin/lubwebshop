878636046b4862a1d68b871b28f2a435344eca8b
sylvain lubwimi <lubwimi@gmail.com>
2022-08-19 22:27:59 +0200

    Create-React-App

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <header>
+        <a href="/">lubwebshop</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+};
+
+export default App;
