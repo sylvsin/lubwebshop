4e9a64aca563d4a2621267e594a0620b8ea4c893
sylvain lubwimi <lubwimi@gmail.com>
2022-08-20 21:43:28 +0200

    List Products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 
+import data from './data';
+
 const App: React.FC = () => {
   return (
     <div>
       <header>
         <a href="/">lubwebshop</a>
       </header>
-      <main>list products</main>
+
+      <main>
+        <h1>T-Shirts</h1>
+        <div className="products">
+          {data.products.map((product) => {
+            return (
+              <div className="product" key={product.slug}>
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name} />
+                </a>
+                <div className="product-info">
+                  <a href={`/product/${product.slug}`}>
+                    <p>{product.name}</p>
+                  </a>
+                  <p>
+                    <strong>${product.price}</strong>
+                  </p>
+                  <button>Add to cart</button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </main>
     </div>
   );
 };
